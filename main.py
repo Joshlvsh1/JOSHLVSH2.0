7b1c72ead8e011fffab90125333dfdfe5a913d55
Josh <joshuae16@icloud.com>
2022-07-14 21:37:47 +0100

    no message

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print ("hello")
-print ("yh whatever")
+import time
+def countdown(t):
+    while t > 0:
+        print(t)
+        t -= 1
+        time.sleep(1)
+    print("BLAST OFF")
+
+print("how many seconds to count down int:")
+seconds = input()
+while not seconds.isdigit():
+    print("Nahhhh bro not that")
+    seconds = input()
+seconds = int(seconds)
+countdown(seconds)
 
